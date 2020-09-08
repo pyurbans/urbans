b86088dea7055c85211b9388a16279a87c94328b
patrick <me@patrickphat.com>
2020-09-08 16:02:56 +0000

    fix :bug:

--- urbamt/__init__.py
+++ urbamt/__init__.py
@@ -1,8 +1,3 @@
 """Import production-ready tools of URBaMT"""
-try:
-    from .translator import Translator
-except Exception as e:
-    print(e)
-    pass
-else: 
-    __all__.append("Translator")
+from .translator import Translator
+
--- urbamt/utils/__init__.py
+++ urbamt/utils/__init__.py
@@ -1,3 +1,3 @@
-    """
-    Utilities for tree manipulation
-    """
+"""
+Utilities for tree manipulation
+"""
