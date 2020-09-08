6f0e8c3c55bb2fe8da79c374bdb1373d4e6a6198
patrick <me@patrickphat.com>
2020-09-08 15:50:40 +0000

    fix codacy issues

--- urbamt/__init__.py
+++ urbamt/__init__.py
@@ -1,3 +1,8 @@
-from .translator import Translator
-
-__all__ = [Translator]
+"""Import production-ready tools of URBaMT"""
+try:
+    from .translator import Translator
+except Exception as e:
+    print(e)
+    pass
+else: 
+    __all__.append("Translator")
