9d49fd1297f140fe08fa36ec51189f5efd3fb5f8
Patrick_Cin <me@patrickphat.com>
2020-09-09 00:24:14 +0700

    Handle codacy issue (#17)
    * handle codacy issue
    
    * remove blank space docstring

--- urbamt/utils/tree_manipulation.py
+++ urbamt/utils/tree_manipulation.py
@@ -9,7 +9,7 @@ def tree_to_ptree(tree: nltk.Tree):
     return ptree
 
 def swap_tree_given_left(left_tree: nltk.Tree, displacement: List[int], new_words= List[str]):
-    """swap left node with right node within a parent node."""
+    """Swap left node with right node within a parent node."""
     nodes = [left_tree]
     right_tree = left_tree.right_sibling()
     parent_tree = left_tree.parent()
@@ -36,7 +36,7 @@ def swap_tree_given_left(left_tree: nltk.Tree, displacement: List[int], new_word
             parent_tree.append(new_word)
         else:
             parent_tree.append(nodes[disp])
-        
+
     return parent_tree
     
 
@@ -114,10 +114,7 @@ def translate_trees_grammar(list_trees: List[nltk.Tree], src_to_tgt_grammar, src
     return max(trans_map, key=trans_map.get)
 
 def calculate_displacement(src_grammar, tgt_grammar):
-    """
-    Calculate displacement between 2 grammar:
-        E.g: S -> A B C to S -> B C A has displacement of [1 2 0]
-    """
+    """Calculate displacement between 2 grammar. E.g: S -> A B C to S -> B C A has displacement of [1 2 0]"""
     src_grammar_lst = src_grammar.split()
     tgt_grammar_lst = tgt_grammar.split()
     
